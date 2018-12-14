dc329b60e87fef7c18a0f1174bdb908cab3df3d2
andreas <andreas.peter.ch@gmail.com>
2018-12-14 18:45:32 +0000

    Clean up day 11

--- day11/day11.jl
+++ day11/day11.jl
@@ -9,17 +9,22 @@ end
 
 function ap_power(gsn; ns = [3])
     accgrd = accumulate(+, accumulate(+, grid(gsn), dims=1), dims=2)
-    pval(x,y,n) = accgrd[x+n,y+n] + accgrd[x,y] - accgrd[x,y+n] - accgrd[x+n,y]
-    maxpars = (1,1,1,1)
+    pval((x,y),n) = accgrd[x+n,y+n] + accgrd[x,y] - accgrd[x,y+n] - accgrd[x+n,y]
+    maxpars = (1, CartesianIndex(1,1), 1)
     for n in ns
-        for y in 1:300-n, x in 1:300-n
-            pv = pval(x,y,n)
-            pv > first(maxpars) && (maxpars = (pv,x+1,y+1,n))
+        for I in CartesianIndices((1:300-n, 1:300-n))
+            pv = pval(I.I,n)
+            pv > first(maxpars) && (maxpars = (pv,I,n))
         end
     end
     return maxpars
 end
 
-using BenchmarkTools
-@btime ap_power(1788,ns=1:300)
-@time (ap_power(1788,ns=1:300), ap_power(1788,ns=3))
+day11_1() = ap_power(1788, ns=3)[2].I .+ 1
+function day11_2()
+    (pv, cinds, n) = ap_power(1788, ns=1:300)
+    return ((cinds.I .+ 1)..., n)
+end
+
+day11_1()
+day11_2()
