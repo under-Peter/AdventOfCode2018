f781d945e71e8bcbb22c5f49f3788fa2a6b08472
andreas <andreas.peter.ch@gmail.com>
2018-12-14 16:07:51 +0000

    Recipes accept Int8 now for less memory consumption and speedups

--- day14/day14.jl
+++ day14/day14.jl
@@ -1,41 +1,47 @@
-mutable struct Recipes{T}
+mutable struct Recipes{T,S}
     v::T
     l::Int
-    Recipes(v) = new{typeof(v)}(v, length(v))
+    function Recipes(v,l=length(v))
+        lv = length(v)
+        v = copy(v)
+        resize!(v, l)
+        return new{typeof(v), eltype(v)}(v, lv)
+    end
 end
 
-Base.eltype(p::Recipes) = eltype(p.v)
+Base.eltype(p::Recipes{T,S}) where {S,T} = S
 Base.getindex(rs::Recipes,i) = rs.v[i]
 function Base.push!(rs::Recipes, n)
-    resize!(rs, rs.l+1)
+    grow!(rs, rs.l+1)
     rs.v[rs.l] = n
     return nothing
 end
 Base.length(rs::Recipes) = rs.l
 Base.show(io::IO,rs::Recipes) = show(io, rs.v[1:rs.l])
 
-function Base.resize!(rs::Recipes, l)
+function grow!(rs::Recipes, l)
     l > length(rs.v) && Base.resize!(rs.v, l * 2)
     rs.l = l
 end
 
-function addrecipes!(rs, elfs, escore)
+function addrecipes!(rs, elfs)
     nelfs = length(elfs)
-    for i in 1:nelfs;
-        escore[i] = rs[elfs[i]]
-    end
-    d, r = divrem(sum(escore),10)
+    e1, e2 = elfs[1], elfs[2]
+    es1, es2 = rs[e1], rs[e2]
+    d, r = divrem(es1 + es2, eltype(rs)(10))
     d != 0 && push!(rs,d)
     push!(rs, r)
     l = length(rs)
-    @. elfs = mod1(elfs + escore + 1, l)
+    elfs[1] = mod1(elfs[1] + es1 + 1,l)
+    elfs[2] = mod1(elfs[2] + es2 + 1,l)
+    return ifelse(iszero(d), 1, 2)
 end
 
-function day14_1(nr=9, init=[3,7], nelfs=2)
-    elfs, escore = collect(1:nelfs), zeros(Int,nelfs)
-    rs = Recipes(init)
+function day14_1(nr=9, init=Int8[3,7], nelfs=2)
+    elfs = collect(1:nelfs)
+    rs = Recipes(init, nr+10+1)
     while length(rs) < nr + 10 + 1
-        addrecipes!(rs, elfs, escore)
+        addrecipes!(rs, elfs)
     end
     join(map(string,rs.v[(1:10) .+ nr]))
 end
@@ -54,17 +60,17 @@ function fastcompare(rs, dinput, inds)
     return true
 end
 
-function day14_2(input, init=[3,7], nelfs=2)
-    dinput = parse.(Int,collect(input))
+function day14_2(input, init=Int8[3,7], nelfs=2)
+    dinput = parse.(Int8,collect(input))
     len = length(dinput)
-    elfs, escore = collect(1:nelfs), zeros(Int,nelfs)
+    elfs = collect(1:nelfs)
     rs = Recipes(init)
     while true
-        addrecipes!(rs, elfs, escore)
+        n = addrecipes!(rs, elfs)
         if length(rs) > len+1
             inds = (length(rs)-len+1):length(rs)
             fastcompare(rs, dinput, inds) && return (length(rs)-len)
-            fastcompare(rs, dinput, inds .- 1) && return (length(rs)-len-1)
+            (n==2) && fastcompare(rs, dinput, inds .- 1) && return (length(rs)-len-1)
         end
     end
     error()
@@ -75,4 +81,4 @@ day14_2("01245") == 5
 day14_2("92510") == 18
 day14_2("59414") == 2018
 
-@time day14_2("540561")
+day14_2("540561") == 20254833
